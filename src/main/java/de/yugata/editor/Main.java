94d1efbc269d38817ab80032ad05176b4d217efe
callisto-jovy <romanportius@gmail.com>
2023-09-08 20:52:04 +0200

    Draw Number of segments on the frame

--- src/main/java/de/yugata/editor/Main.java
+++ src/main/java/de/yugata/editor/Main.java
@@ -4,20 +4,24 @@ import de.yugata.editor.editor.AudioAnalyser;
 import de.yugata.editor.editor.Editor;
 import de.yugata.editor.model.CLIArgs;
 import de.yugata.editor.playback.VideoPlayer;
+import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacv.FFmpegLogCallback;
+import org.bytedeco.opencv.opencv_java;
 import picocli.CommandLine;
 
 public class Main {
 
 
     public static void main(String... args) {
-        final CLIArgs cliArgs = CommandLine.populateSpec(CLIArgs.class, args);
-
+        Loader.load(opencv_java.class);
         FFmpegLogCallback.set();
+
+        final CLIArgs cliArgs = CommandLine.populateSpec(CLIArgs.class, args);
         //Analyse the audio
         final AudioAnalyser audioAnalyser = new AudioAnalyser(cliArgs.getAudioInput());
         audioAnalyser.analyseBeats(cliArgs.getPeakThreshold());
 
+        // Create the video player & set callback
         final VideoPlayer videoPlayer = new VideoPlayer(cliArgs.getInput(), (timestamps) -> {
             final Editor editor = new Editor(cliArgs.getInput(), cliArgs.getAudioInput(), audioAnalyser.getTimeBetweenBeats(), timestamps);
             editor.edit();
--- src/main/java/de/yugata/editor/playback/VideoThread.java
+++ src/main/java/de/yugata/editor/playback/VideoThread.java
@@ -1,20 +1,31 @@
 package de.yugata.editor.playback;
 
-import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import static org.opencv.imgproc.Imgproc.FONT_HERSHEY_DUPLEX;
+import static org.opencv.imgproc.Imgproc.FONT_HERSHEY_PLAIN;
 
 public class VideoThread extends Thread {
 
     private final FFmpegFrameGrabber frameGrabber;
 
-    private final CanvasFrame parent;
+    private final VideoPlayer parent;
 
     private boolean paused;
 
-    public VideoThread(final String videoInput, final CanvasFrame parent) {
+    public VideoThread(final String videoInput, final VideoPlayer parent) {
         this.parent = parent;
         this.frameGrabber = new FFmpegFrameGrabber(videoInput);
+        frameGrabber.setOption("allowed_extensions", "ALL");
+        frameGrabber.setOption("hwaccel", "cuda");
+        frameGrabber.setVideoCodecName("hevc_cuvid");
+
     }
 
     @Override
@@ -27,13 +38,23 @@ public class VideoThread extends Thread {
         super.start();
     }
 
+
+    private final OpenCVFrameConverter<Mat> javaCVConv = new OpenCVFrameConverter.ToMat();
+    private final Point textOrigin = new Point(50, 50);
+    private final Scalar scalarWhite = new Scalar(255, 255, 255);
+
     @Override
     public void run() {
         super.run();
         try {
             Frame frame;
-            while ((frame = frameGrabber.grab()) != null && !interrupted()) {
-                parent.showImage(frame);
+            while ((frame = frameGrabber.grabImage()) != null && !interrupted()) {
+
+                final org.opencv.core.Mat coreFrameMat = javaCVConv.convertToOrgOpenCvCoreMat(frame);
+
+                Imgproc.putText(coreFrameMat, "Segments: " + parent.timeStamps.size(), textOrigin, FONT_HERSHEY_PLAIN, 4, scalarWhite);
+
+                parent.showImage(javaCVConv.convert(coreFrameMat));
 
                 synchronized (this) {
                     if (paused) {
