4a18230d903e63f1dd0f4a4829583a9eae23a5a0
callisto-jovy <48448458+callisto-jovy@users.noreply.github.com>
2023-09-19 13:00:32 +0200

    Editing Flags now have values they may use

--- src/main/java/de/yugata/editor/commands/EditingOptionsCommand.java
+++ src/main/java/de/yugata/editor/commands/EditingOptionsCommand.java
@@ -7,11 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.shell.command.CommandRegistration;
 import org.springframework.shell.standard.ShellComponent;
 
-import javax.swing.text.html.ListView;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @ShellComponent
 public class EditingOptionsCommand {
@@ -29,6 +27,11 @@ public class EditingOptionsCommand {
                 .description("Set a flag for rendering the edit.")
                 .and()
                 .withOption()
+                .longNames("value")
+                .type(Integer.class)
+                .description("Set the flag's value if possible.")
+                .and()
+                .withOption()
                 .longNames("see")
                 .description("Prints the state of all the editing flags.")
                 .and()
@@ -40,24 +43,38 @@ public class EditingOptionsCommand {
                 .description(DESCRIPTION)
                 .withTarget()
                 .function(ctx -> {
-
                     if (ctx.hasMappedOption("set")) {
                         final String set = ctx.getOptionValue("set");
-                        Editor.INSTANCE.addOrRemoveFlag(EditingFlag.valueOf(set));
 
-                        return String.format("Added flag %s to existing flags!", set);
+                        if (ctx.hasMappedOption("value")) {
+                            final int value = ctx.getOptionValue("value");
+
+                            EditingFlag.valueOf(set).setSetting(value);
+                            return String.format("Set flag %s to value %d", set, value);
+                        } else {
+                            Editor.INSTANCE.addOrRemoveFlag(EditingFlag.valueOf(set));
+                            return String.format("Added flag %s to existing flags!", set);
+                        }
+
                     } else if (ctx.hasMappedOption("see")) {
-                        return Editor.INSTANCE.getEditingFlags();
+                        return formatFlags(Editor.INSTANCE.getEditingFlags().stream());
                     } else if (ctx.hasMappedOption("all")) {
-                        return Arrays.stream(EditingFlag.values())
-                                .map(editingFlag -> editingFlag.name() + " : " + editingFlag.getDescription())
-                                .collect(Collectors.joining("\n"));
+                        return formatFlags(Arrays.stream(EditingFlag.values()));
                     }
-
                     return "Option not found.";
                 })
                 .and()
                 .build();
+    }
+
+    private String formatFlags(final Stream<EditingFlag> flags) {
+        return flags
+                .map(this::formatFlag)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String formatFlag(final EditingFlag flag) {
+        return String.format("%s : %s : Value: %d", flag.name(), flag.getDescription(), flag.getSetting());
     }
 
 }
