aabbc032b992fde68a65303193af4ae24aaf57b4
callisto-jovy <romanportius@gmail.com>
2023-10-10 17:11:17 +0200

    Add editing flags from config

--- src/main/java/de/yugata/editor/editor/EditingFlag.java
+++ src/main/java/de/yugata/editor/editor/EditingFlag.java
@@ -3,7 +3,7 @@ package de.yugata.editor.editor;
 public enum EditingFlag {
 
     WRITE_HDR_OPTIONS("Writes HDR tags to the video options. Should only be used with HDR content.", -1),
-    BEST_QUALITY("Will use the best quality preset by FFMPEG, i.e. cf=0", 0),
+    BEST_QUALITY("Will use the best quality preset by FFMPEG, i.e. cf=0", 12),
     SHUFFLE_SEQUENCES("Shuffles the video timestamps, which may lead to more variety in the end product. If you selected your scenes with care and they follow a particular order, this should be disabled.", -1),
     INTERPOLATE_FRAMES("Will interpolate and blend between the frames, giving the illusion of motion blur. This is not GPU-accelerated and might take a long time.", 120),
     FADE_TRANSITION("Will fade in from black for a certain amount of milliseconds", 200),
