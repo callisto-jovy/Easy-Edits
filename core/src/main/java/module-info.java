f4b5a1abae45f9d6a8d01db8ec10dd6529339167
callisto-jovy <romanportius@gmail.com>
2023-10-14 17:57:45 +0200

    Add module-info.java

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module Easy.Edits {
+
+
+    exports de.yugata.easy.edits.editor;
+    exports de.yugata.easy.edits.audio;
+
+
+    requires java.logging;
+    requires java.desktop;
+    requires com.google.gson;
+    requires org.bytedeco.javacv;
+}
